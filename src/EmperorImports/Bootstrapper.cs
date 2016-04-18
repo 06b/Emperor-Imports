446ca0e307e02013b89c036c22bf84194a9a3a6f
Adrian D. Alvarez <adrian@adriandalvarez.com>
2016-04-18 19:12:27 -0400

    Add unsecured elmah logging

---
+++ src/EmperorImports/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Elmah;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace EmperorImports
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+            /// <summary>
+            /// Elmah Logging - Enable Exception logging with select HttpStatusCode logging
+            /// </summary>
+
+            //Note: Elmah left unsecured purposely.
+            Elmahlogging.Enable(pipelines, "elmah", new string[0], new HttpStatusCode[] { HttpStatusCode.NotFound, HttpStatusCode.InsufficientStorage, });
+        }
+
+    }
+}
