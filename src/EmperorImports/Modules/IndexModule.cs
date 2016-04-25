161aa4e720984bad270293fdf930f1e5e7ed38ce
Adrian D. Alvarez <adrian@adriandalvarez.com>
2016-04-24 22:13:33 -0400

    Add WhipPress login pages

--- src/EmperorImports/Modules/IndexModule.cs
+++ src/EmperorImports/Modules/IndexModule.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using Nancy.Responses;
 
 namespace EmperorImports.Modules
 {
@@ -12,6 +13,12 @@ namespace EmperorImports.Modules
             };
 
             Get["/credits"] = _ => { return View["credits"]; };
+
+            Get["/wp-admin/"] = _ => { return Response.AsRedirect("/wp-admin.php", RedirectResponse.RedirectType.SeeOther); };
+
+            Get["/wp-admin.php"] = _ => { return View["admin/login"]; };
+
+            Post["/wp-admin.php"] = _ => { return "submitted"; };
         }
     }
 }
